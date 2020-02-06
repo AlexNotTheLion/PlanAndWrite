fa216bcbc5302ca27b06a9b0053cacddc1b0da54
AlexNotTheLion <s5006039@bournemouth.ac.uk>
2020-02-06 14:57:50 +0000

    rake progression

--- build/main.py
+++ build/main.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-from rake_nltk import Rake
+from rake_nltk import Rake, Metric
 
 import functools
 
@@ -8,19 +8,16 @@ import numpy as np
 
 import tensorflow as tf
 
-r = Rake()
+r = Rake(max_length = 2)
 
 train_file_path = "ROCStories.csv"
 
-np.set_printoptions(precision = 3, suppress=True)
-
-label_column = 'storyid'
 
 def get_dataset(file_path, **kwargs):
     dataset = tf.data.experimental.make_csv_dataset(
         file_path,
         batch_size=1,
-        label_name = label_column,
+        label_name = None,
         na_value="",
         num_epochs=1,
         ignore_errors=True,
@@ -29,10 +26,23 @@ def get_dataset(file_path, **kwargs):
     return dataset
 
 def show_batch(dataset):
-    for batch, label in dataset.take(1):
+    for batch in dataset.take(1):
         for key, value in batch.items():
-            print("{:20s}: {}".format(key, value.numpy()))
+            sentence = value.numpy()
+            sentence = np.array([x.decode() for x in sentence])#convert from array string
+            sentence = str("{}".format(sentence))#convert string to utf8
+            sentence = sentence[2:-2]#remove [' characteters from string
+            out = str("{:12s}: {}".format(key, sentence))
+            print(out)
+
+            if (key == "storyid" or key == "storytitle"):
+                continue
+
+            r.extract_keywords_from_text(sentence)#extract most important word from given text
+            print("best ranked word: {}".format(r.get_ranked_phrases_with_scores()))
+            print("\n")
         print("\n")
+
 
 csv_columns = ['storyid', 'storytitle', 'sentence1', 'sentence2', 'sentence3', 'sentence4', 'sentence5']
 
