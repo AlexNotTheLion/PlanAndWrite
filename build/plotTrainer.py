4803e724e64fe2db9f5ee79fcc02bbe3c13d63d2
AlexNotTheLion <s5006039@bournemouth.ac.uk>
2020-02-15 21:28:32 +0000

    tokenizer and word count

--- build/plotTrainer.py
+++ build/plotTrainer.py
@@ -4,13 +4,17 @@ import functools
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
+import tensorflow_datasets as tfds
+from keras_preprocessing.text import Tokenizer
 
 train_file_path = "storyplot.csv"
 
+docs=[]
+
 def get_dataset(file_path, **kwargs):
     dataset = tf.data.experimental.make_csv_dataset(file_path,
-        batch_size=5,
-        label_name = None,
+        batch_size=1,
+        label_name = 'storyid',
         na_value="",
         num_epochs=1,
         ignore_errors=True,
@@ -19,30 +23,43 @@ def get_dataset(file_path, **kwargs):
     return dataset
 
 def show_batch(dataset):
-    for batch in dataset.take(1):
-        for key, value in batch.items():
-            sentence = value.numpy()
-            sentence = np.array([x.decode() for x in sentence])#convert from array string
-            sentence = str("{}".format(sentence))#convert string to utf8
-            sentence = sentence[2:-2]#remove [' characteters from string
-            print(str("{:12s}: {} \n".format(key, sentence)))
+  for batch, label in dataset.take(2):
+    for key, value in batch.items():
+        # sentence = value.numpy()
+        # sentence = np.array([x.decode() for x in sentence])#convert from array string
+        # sentence = str("{}".format(sentence))#convert string to utf8
+        # sentence = sentence[2:-2]#remove [' characteters from string
+        # print(sentence)
+        # docs.append(sentence)
+        print("{:20s}: {}".format(key,value))
 
 
-csv_columns = ['storytitle', 'key 1', 'key 2', 'key 3', 'key 4', 'key 5']
+def pack(features, labels):
+    return tf.stack(list(features.values()), axis = -1), labels
+
+csv_columns = ['storyid', 'storytitle', 'key 1', 'key 2', 'key 3', 'key 4', 'key 5']
 
 storyplot_dataset = get_dataset(train_file_path, select_columns=csv_columns)
 
 show_batch(storyplot_dataset)
 
 
+# t = Tokenizer()
+# t.fit_on_texts(docs)
+# print(len(t.word_index)+1)
+
+#29219 words for storyplot csv
+#38617 for full data
+
+#TODO build tokenizer for data
+#TODO get vocab size
+
+#TODO split dataset into training and testing / evaluation
+#TODO build plot planning model train and test
+
 #just for storyline planning
 #seq2seq conditional generation model
 #encodes title into a vector using a bidirectional long short-term memory
 #network
 #generates words in the storyline using another single-directional LSTM
-
-
-#(train_data, train_labels), (test_data, test_labels) = storyplot_dataset.load_data(num_words=100000)
-
-#print(train_data[0])
 
