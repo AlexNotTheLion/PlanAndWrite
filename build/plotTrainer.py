a1631ebd05991bd1f70766eec94aeef51d63b8c4
AlexNotTheLion <s5006039@bournemouth.ac.uk>
2020-03-05 21:47:59 +0000

    changed how dataset is built and tokenisation is done

--- build/plotTrainer.py
+++ build/plotTrainer.py
@@ -21,7 +21,7 @@ with open(csv_file_path) as f:
 def get_dataset(file_path, **kwargs):
     dataset = tf.data.experimental.make_csv_dataset(file_path,
         batch_size=1,
-        label_name = 'storyid',
+        label_name = None,
         na_value="",
         num_epochs=1,
         ignore_errors=True,
@@ -29,37 +29,79 @@ def get_dataset(file_path, **kwargs):
         **kwargs)
     return dataset
 
-# def show_batch(dataset):
-    # for batch, label in dataset.take(2):
-    #     for key, value in batch.items():
-    #     # sentence = value.numpy()
-    #     # sentence = np.array([x.decode() for x in sentence])#convert from array string
-    #     # sentence = str("{}".format(sentence))#convert string to utf8
-    #     # sentence = sentence[2:-2]#remove [' characteters from string
-    #     # print(sentence)
-    #     # docs.append(sentence)
-    #         print("{:20s}: {}".format(key,value))
-
-
 def pack(features, labels):
-    return tf.stack(list(features.values()), axis = 1), labels
-
+    return tf.stack(list(features.values()), axis = 1), labels    
 
 csv_columns = ['storyid', 'storytitle', 'key1', 'key2', 'key3', 'key4', 'key5']
 
-storyplot_dataset = get_dataset(csv_file_path, select_columns=csv_columns)
+# storyplot_dataset = get_dataset(csv_file_path, select_columns=csv_columns)
 
-packed_dataset = storyplot_dataset.map(pack)
+##EXPERIMENT
 
+data = []
 
 print("loading vocab from csv")
 df = pd.read_csv(csv_file_path, usecols=[1,2,3,4,5,6], encoding="utf-8")
 vocab = df[['storytitle', 'key1', 'key2', 'key3', 'key4', 'key5']].agg(' '.join, axis=1).tolist()
+sub_set = df[['storytitle','key1']]
+data = list(sub_set.itertuples(index=False, name =None))#build list of tuples of story title followed by first sentence 
 print("file loaded")
+
+#build dataset from data
+dataset = tf.data.Dataset.from_tensor_slices(data)
 
 t = Tokenizer()
 t.fit_on_texts(vocab)
 print("token size : {}".format(len(t.word_index)))
+
+print("please enter a title:")
+string = input()
+
+#build word sequence from string / input
+seq = tf.keras.preprocessing.text.text_to_word_sequence(
+    string, filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n', lower=True, split= ' '
+)
+
+#turn word sequence into tokens
+new_seq = t.texts_to_sequences(seq)
+#tokens to words
+new_string = t.sequences_to_texts(new_seq)
+
+for token, work in zip(new_seq, new_string):
+    print("token: {} ----> {}".format(token, work))
+
+###END
+
+# print(type(storyplot_dataset))
+
+# packed_dataset = storyplot_dataset.map(pack)
+#maybe build dataset manually rather than get_dataset function
+
+#model fit can require a td.data dataset which should return a tuple of (inputs, targets) , (titles, plots) 
+
+# for features, labels in packed_dataset.take(1):
+#     print(features)
+#     print(labels)  
+
+# print("loading vocab from csv")
+# df = pd.read_csv(csv_file_path, usecols=[1,2,3,4,5,6], encoding="utf-8")
+# vocab = df[['storytitle', 'key1', 'key2', 'key3', 'key4', 'key5']].agg(' '.join, axis=1).tolist()
+# print("file loaded")
+
+
+# t = Tokenizer()
+# t.fit_on_texts(vocab)
+# print("token size : {}".format(len(t.word_index)))
+
+#data needs to be split into two np arrays, titles as inputs and sentences as outputs
+#these need to be tokenized arrays
+#history = model.fit(input, output, epochs, **kwargs)
+#model.predict(["test title"]) then print the predicted stroyplot
+# a dense layer is a layer which all ouputs go to all the different following nodes in the following layer
+
+#features = inputs to the model (title)
+#labels = outputs (generated storyline!)
+#training example is a pair of the features and labels
 
 # b = Tokenizer()
 # b.fit_on_texts(vocab2)
