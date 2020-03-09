6d76b511c9aefc74a2fd0faaaa1e25f31571f451
AlexNotTheLion <s5006039@bournemouth.ac.uk>
2020-03-09 20:43:14 +0000

    changed loading of data, tokenization and data management and now have a working dataset

--- build/plotTrainer.py
+++ build/plotTrainer.py
@@ -4,73 +4,64 @@ import functools
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras.preprocessing.text import Tokenizer
 import pandas as pd
 
 csv_file_path = "storyplot.csv"
-vocab_file = "vocab.txt"
-
-getVocab = False
-storeData = False
 
 vocab = []
 
-with open(csv_file_path) as f:
-    csvLen = sum(1 for row in f)
+def tokenize(lang):
+    tensor = word_tokenizer.texts_to_sequences(lang)
+    tensor = tf.keras.preprocessing.sequence.pad_sequences(tensor, padding='post')
 
-def get_dataset(file_path, **kwargs):
-    dataset = tf.data.experimental.make_csv_dataset(file_path,
-        batch_size=1,
-        label_name = None,
-        na_value="",
-        num_epochs=1,
-        ignore_errors=True,
-        shuffle=False,
-        **kwargs)
-    return dataset
+    return tensor
 
-def pack(features, labels):
-    return tf.stack(list(features.values()), axis = 1), labels    
+def load_dataset(data):
+    features, labels = zip(*data)
+    
+    input_tensor = tokenize(features)
+    target_tensor = tokenize(labels)
 
-csv_columns = ['storyid', 'storytitle', 'key1', 'key2', 'key3', 'key4', 'key5']
+    return input_tensor, target_tensor
 
-# storyplot_dataset = get_dataset(csv_file_path, select_columns=csv_columns)
+def convert(lang, tensor):
+    for t in tensor:
+        if t!= 0:
+            print ("%d ----> %s" % (t, word_tokenizer.index_word[t]))
 
-##EXPERIMENT
-
-data = []
 
 print("loading vocab from csv")
-df = pd.read_csv(csv_file_path, usecols=[1,2,3,4,5,6], encoding="utf-8")
+df = pd.read_csv(csv_file_path, usecols=[1,2,3,4,5,6], encoding="utf-8", nrows=10)
 vocab = df[['storytitle', 'key1', 'key2', 'key3', 'key4', 'key5']].agg(' '.join, axis=1).tolist()
 sub_set = df[['storytitle','key1']]
 data = list(sub_set.itertuples(index=False, name =None))#build list of tuples of story title followed by first sentence 
 print("file loaded")
 
-#build dataset from data
-dataset = tf.data.Dataset.from_tensor_slices(data)
+word_tokenizer = tf.keras.preprocessing.text.Tokenizer(filters='')
+word_tokenizer.fit_on_texts(vocab)
 
-t = Tokenizer()
-t.fit_on_texts(vocab)
-print("token size : {}".format(len(t.word_index)))
+input_tensor, target_tensor = load_dataset(data)
 
-print("please enter a title:")
-string = input()
+print(input_tensor[0])
 
-#build word sequence from string / input
-seq = tf.keras.preprocessing.text.text_to_word_sequence(
-    string, filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n', lower=True, split= ' '
-)
+convert(word_tokenizer, input_tensor[0])
+print("\noutput tensor")
+convert(word_tokenizer, target_tensor[0])
 
-#turn word sequence into tokens
-new_seq = t.texts_to_sequences(seq)
-#tokens to words
-new_string = t.sequences_to_texts(new_seq)
+dataset = tf.data.Dataset.from_tensor_slices((input_tensor, target_tensor))
 
-for token, work in zip(new_seq, new_string):
-    print("token: {} ----> {}".format(token, work))
+for i in dataset.take(1):
+    print(i)
+
+##organise into features and tensors before slcices https://www.tensorflow.org/api_docs/python/tf/data/Dataset#from_tensor_slices
 
 ###END
+
+
+#meeting discussion
+#build zipped dataset of input, expected output
+#pass to embedding layer and follow https://www.tensorflow.org/tutorials/text/nmt_with_attention
+#then try some seq 2 seq stuff
 
 # print(type(storyplot_dataset))
 
